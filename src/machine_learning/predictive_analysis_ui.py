bc41cbbb09201d6a402b9a3203a9fc45d5f74e16
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-10-30 15:35:51 +0000

    update

--- src/machine_learning/predictive_analysis_ui.py
+++ src/machine_learning/predictive_analysis_ui.py
@@ -44,10 +44,19 @@ def predict_tenure(X_live, tenure_features, tenure_pipeline, tenure_labels_map):
 	proba = tenure_prediction_proba[0,tenure_prediction][0]*100
 	tenure_levels = tenure_labels_map[tenure_prediction[0]]
 
-	statement = (
-		f"* In addition, there is a {proba.round(2)}% probability the prospect "
-		f"will stay **{tenure_levels} months**. ")
-
+	if tenure_prediction != 1:
+		statement = (
+			f"* In addition, there is a {proba.round(2)}% probability the prospect "
+			f"will stay **{tenure_levels} months**. "
+			)
+	else:
+		statement = (
+			f"* The model has predicted the prospect would stay **{tenure_levels} months**, "
+			f"however we acknowledge that the recall and precision levels for {tenure_levels} is not "
+			f"strong. The AI tends to identify potential churners, but for this prospect ir is not "
+			f"confident enough on how long the prospect would stay."
+		)
+		
 	st.write(statement)
 
 
