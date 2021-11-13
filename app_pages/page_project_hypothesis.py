b02231beedff9067483b25b7437f5d564e9dfa02
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-11-13 09:58:32 +0000

    update

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -4,7 +4,7 @@ def page_project_hypothesis_body():
 
     st.write("### Project Hypothesis and Validation")
 
-
+    # conclusions taken from "02 - Churned Customer Study" notebook 
     st.success(
         f"* We suspect customers are churning with low tenure levels: Correct. "
         f"The correlation study at Churned Customer Study supports that. \n\n"
