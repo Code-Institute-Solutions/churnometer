dc65d26a63c807d1912ef8cdc121eaef073d9ab6
NielMc <niel@codeinstitute.net>
2022-11-07 15:28:01 +0000

    pep8 of ml files

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -3,24 +3,26 @@ import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix
 
 # code copied from "Modeling and Evaluation" notebooks
-def confusion_matrix_and_report(X,y,pipeline,label_map):
 
-  prediction = pipeline.predict(X)
 
-  st.write('#### Confusion Matrix')
-  st.code(pd.DataFrame(confusion_matrix(y_true=prediction, y_pred=y),
-        columns=[ ["Actual " + sub for sub in label_map] ], 
-        index= [ ["Prediction " + sub for sub in label_map ]]
-        ))
+def confusion_matrix_and_report(X, y, pipeline, label_map):
 
-  st.write('#### Classification Report')
-  st.code(classification_report(y, prediction, target_names=label_map),"\n")
+    prediction = pipeline.predict(X)
+
+    st.write('#### Confusion Matrix')
+    st.code(pd.DataFrame(confusion_matrix(y_true=prediction, y_pred=y),
+                         columns=[["Actual " + sub for sub in label_map]],
+                         index=[["Prediction " + sub for sub in label_map]]
+                         ))
+
+    st.write('#### Classification Report')
+    st.code(classification_report(y, prediction, target_names=label_map), "\n")
 
 
 # code copied from "Modeling and Evaluation" notebooks
-def clf_performance(X_train,y_train,X_test,y_test,pipeline,label_map):
-  st.info("Train Set")
-  confusion_matrix_and_report(X_train,y_train,pipeline,label_map)
+def clf_performance(X_train, y_train, X_test, y_test, pipeline, label_map):
+    st.info("Train Set")
+    confusion_matrix_and_report(X_train, y_train, pipeline, label_map)
 
-  st.info("Test Set")
-  confusion_matrix_and_report(X_test,y_test,pipeline,label_map)
+    st.info("Test Set")
+    confusion_matrix_and_report(X_test, y_test, pipeline, label_map)
